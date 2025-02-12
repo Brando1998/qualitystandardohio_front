0d206c2cbb7a234fdcdabc9de6ab8b916925dd0b
Brando1998 <brandodiazmont@gmail.com>
2025-02-12 10:20:19 -0500

    Migrate from render to heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 //environment.ts 
 export const environment = {
-    production: true,
-    apiUrl: 'http://localhost:8000/api',
+    production: false,
+    apiUrl: 'https://qualitystandardohio-d48cccb8c93b.herokuapp.com/api',
     version: new Date().getTime()
 };
