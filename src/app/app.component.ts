099c7ae83a1117d43efe90b1420b1d566b50c976
Brando1998 <brandodiazmont@gmail.com>
2025-02-12 09:16:09 -0500

    fix cache

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LocalStorageService } from './services/localstorage.service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'front_qualitystandardohio';
+
+  constructor(
+    private http: HttpClient,
+    private localStorageService: LocalStorageService
+  ) {
+    this.checkVersion();
+  }
+
+  checkVersion() {
+    if (this.localStorageService.isLocalStorageAvailable()) {
+      this.http.get('version.json?' + new Date().getTime()).subscribe((data: any) => {
+        const currentVersion = localStorage.getItem('appVersion');
+        if (currentVersion && currentVersion !== data.version) {
+          localStorage.setItem('appVersion', data.version);
+          location.reload(); // Fuerza la recarga cuando hay una nueva versión
+        } else {
+          localStorage.setItem('appVersion', data.version);
+        }
+      });
+    }
+  }
 }
